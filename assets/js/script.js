2365283001b3096c548bf6d8636e0c055df19d14
Grant King <grantryan.king@gmail.com>
2023-10-13 12:59:22 -0700

    Remove pricing calculator section
    Remove pricing calculator section Html, css, and js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,54 +48,6 @@ $(document).ready(function () {
     });
 
 
-  // Pricing calculator
-  $(document).ready(function () {
-    // define rate and speed factor
-    var initialRate = 75;
-    var timeSavingsFactor = 0.7;
-    var rateIncrease = 0.05 / 52;
-
-    // get values from input fields
-    const hoursInput = $('#hours');
-    const totalOutput = $('#total');
-    const calculator = $('#calculator');
-
-    // Calculate estimated mothly cost
-    function calculateCost() {
-      var hours = hoursInput.val();
-      const estimatedHours = hours * timeSavingsFactor;
-      const estimatedCost = (estimatedHours * initialRate).toFixed(2);
-      
-      if (hours > 0) {
-        if (hours > 100) {
-          hours = 100;
-          hoursInput.val(100);
-        }else{
-        $('#pricing-estimate').text(`Based on your usual monthly bookkeeping time of ${hours} hours, Stephen can complete the job in about ${estimatedHours.toFixed(1)} hours, and it will cost approximately $${estimatedCost} per month (30% faster than industry average).`);
-        totalOutput.text("$" + estimatedCost)}
-      } else {
-        hours = 0;
-        hoursInput.val(0);
-        totalOutput.text("$0");
-        $('#pricing-estimate').text(`Type a number in the box above or click the arrows within the box to estimate your monthly bookkeeping cost.`);
-      }
-    }
-
-    // set new background position
-    function scrollBackground() {
-      const hours = hoursInput.val();
-      const newBackgroundPosition = hours * 10;
-      calculator.css('background-position', `bottom 0 right ${newBackgroundPosition}px`);
-    }
-
-    // update cost on input
-    hoursInput.on('input', calculateCost);
-
-    // scroll background on hours input
-    hoursInput.on('input', scrollBackground);
-
-  });
-  
   // Scroll reveal animations
   ScrollReveal().reveal('.section-title', {
     delay: 200,
@@ -121,10 +73,4 @@ $(document).ready(function () {
     interval: 150,
   });
   
-  ScrollReveal().reveal("#calculator", {
-    delay: 200,
-    duration: 800,
-    distance: "50px",
-    origin: "bottom",
-  });
   });
